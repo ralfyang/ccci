4de0f0a9302569dde2e78452fccb78ee9cf763a5
root <root@docker01.dev>
2018-12-26 16:18:07 +0900

    update for the exception of generator

--- 1.quickstart.sh
+++ 1.quickstart.sh
@@ -17,9 +17,26 @@ show_menu(){
 show_menu
 read Menu
 
+
+conf_gen(){
+Host_ip=$1
+	## Create a new docker-compose.yml for the Stack install
+	if [ -f docker-compose.yml.sample ];then
+		cat docker-compose.yml.sample | sed -e "s/HOST_IP/$Host_ip/g" > ./docker-compose.yml
+		echo " docker-compose.yml file has been created as below!"
+		cat docker-compose.yml
+	else
+		echo " docker-compose.yml.sample is not existed here! Please check again."
+		exit 1
+	fi
+}
+
+
 provisioning_docker(){
 	## Create a new keys for worker & web communication
-	./keygen_for_worker.sh
+	if [ ! -f ./keys/web/tsa_host_key.pub ];then
+		./keygen_for_worker.sh
+	fi
 
 	## Host IP address check for the external URL of the Service
 	if [ ! -f /tmp/host_ip ];then
@@ -32,24 +49,37 @@ provisioning_docker(){
 				exit 1
 			else
 				echo "$Host_ip" > /tmp/host_ip
-				## Create a new docker-compose.yml for the Stack install
-				if [ -f docker-compose.yml.sample ];then
-					cat docker-compose.yml.sample | sed -e "s/HOST_IP/$Host_ip/g" > ./docker-compose.yml
-				else
-					echo " docker-compose.yml.sample is not existed here! Please check again."
-					exit 1
-				fi
+				conf_gen $Host_ip
 			fi
+	else
+		Host_ip=$(cat /tmp/host_ip)
+			if [[ $Host_ip = "" ]];then
+				echo "HOST IP address is empty!!. Please check again the IP address of the host."
+				exit 1
+			else
+				conf_gen $Host_ip
+			fi
+	fi
+}
+
+
+checkout(){
+	if [ ! -f docker-compose.yml ];then
+		provisioning_docker
 	fi
 }
 
 clear_setup(){
 	rm -f /tmp/host_ip ./docker-compose.yml
 }
+
+
 	case $Menu in 
 		0) provisioning_docker ;;
-		1) docker-compose up ;;
-		2) docker-compose up -d ;;
+		1) checkout
+		   docker-compose up ;;
+		2) checkout
+		   docker-compose up -d ;;
 		3) docker-compose down ;;
 		RM) clear_setup ;;
 		*) show_menu ;;

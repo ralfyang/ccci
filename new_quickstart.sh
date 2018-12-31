881fe01caeaa02f2c68e672df3c484e215826496
peter.Lee <folimworld@gmail.com>
2018-12-31 08:55:15 +0000

    ED command hidden key, delete .env .consul_*

--- new_quickstart.sh
+++ new_quickstart.sh
@@ -295,7 +295,7 @@ clear_setup(){
 		3) docker-compose down ;;
 		RM) docker-compose down
 		    clear_setup ;;
-		ED) ;;
+		ED) rm -rf .env .consul_* ;;
 		*) echo -e "\033[1;31m Select Menu\033[0m" ;;
 #		*) show_menu ;;
 	esac
